04725e4cdd9697f69673373fc464ca91c181dbe6
Ananda Kelvin <jhousin2002@gmail.com>
2024-11-03 22:59:53 +0700

    fix index

--- server/src/index.js
+++ server/src/index.js
@@ -3,16 +3,12 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import authController from "./auth/auth.controller.js"
 import userController from "./user/user.controller.js"
-import accountController from "./account/accout.controller.js"
-import transactionController from "./transaction/transaction.controller.js"
-import categoryController from "./category/category.controller.js"
-import goalController from "./goal/goal.controller.js"
 
 // import unknownEndPoint from './middleware/unknownEndpoint.js';
 const app = express()
 
 dotenv.config()
-
+const port = 2000
 app.use(express.json())
 app.use(cors())
 
@@ -22,9 +18,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/auth/', authController)
 app.use('/api/', userController)
-app.use('/api/', accountController)
-app.use('/api/', transactionController)
-app.use('/api/', categoryController)
-app.use('/api/', goalController)
-
-module.exports = app;
+app.listen(port, () => {
+  console.log(`BreadFinance is listening on port ${port}`)
+})
